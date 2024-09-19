34976af0c1a2e835bc5a0b79764f783542ed0857
npredein <>
2024-09-19 14:18:44 +0500

    fix: username

--- src/app/modules/user/components/user-info/user-info.component.ts
+++ src/app/modules/user/components/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit } from '@angular/core'
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { MatButton } from '@angular/material/button'
@@ -6,6 +6,7 @@ import { MatFormField, MatLabel } from '@angular/material/form-field'
 import { MatInput } from '@angular/material/input'
 import { Store } from '@ngrx/store'
 import { AuthState } from '../../../auth/models/login.model'
+import { getCurrentUser } from '../../../auth/store/auth.selectors';
 
 @Component({
     selector: 'app-user-info',
@@ -23,14 +24,24 @@ import { AuthState } from '../../../auth/models/login.model'
 })
 export class UserInfoComponent {
     formGroup: FormGroup = new FormGroup({
-        username: new FormControl({ value: null, disabled: true }),
-        password: new FormControl({ value: null, disabled: true }),
-    })
-
+        username: new FormControl({ value: null, disabled: false }),
+        password: new FormControl({ value: null, disabled: false }),
+    });
 
     constructor(
         private store: Store<{ auth: AuthState }>,
-        private destroyRef: DestroyRef
+        private destroyRef: DestroyRef,
     ) {
+
+      this.store.select(getCurrentUser)
+        .pipe(
+          takeUntilDestroyed(this.destroyRef),
+        )
+        .subscribe(userData => {
+          this.formGroup.setValue({
+            username: userData?.username || null,
+            password: userData?.password || null,
+          });
+        });
     }
 }
